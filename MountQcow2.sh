168d5da940ee67ba11d7483efd2db9fe71a550e0
Cedric Vallieu <cvallieu94@gmail.com>
2025-03-21 21:57:58 -0700

    Update MountQcow2.sh
    specify qcow2 format

--- MountQcow2.sh
+++ MountQcow2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo modprobe nbd
-sudo qemu-nbd -c /dev/nbd0 /mnt/android/Pixel-9-AVF-Debian/android.qcow2
+sudo qemu-nbd -c /dev/nbd0 -f qcow2 /mnt/android/Pixel-9-AVF-Debian/android.qcow2
 sudo mount /dev/nbd0p1 /mnt/android
